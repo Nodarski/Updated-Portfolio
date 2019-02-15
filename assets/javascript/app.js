b2704204572f6c4cf5b8a14b9a78b7c5db42446b
nodar <nodarlaw@gmail.com>
2019-02-15 17:02:09 -0500

    added initial files, firebase is working

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+
+var config = {
+    apiKey: "AIzaSyAP0qUViS0XairyYS7uW10dJHuSYlaKPEU",
+    authDomain: "portfolio-a114a.firebaseapp.com",
+    databaseURL: "https://portfolio-a114a.firebaseio.com",
+    projectId: "portfolio-a114a",
+    storageBucket: "portfolio-a114a.appspot.com",
+    messagingSenderId: "436153935525"
+  };
+  firebase.initializeApp(config);
